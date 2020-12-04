4ebf3204241c732b3040f2e1dad565ca1dfe89b1
johnarthurthompson <john.a.thompson@cuanschutz.edu>
2020-12-03 20:23:15 -0700

    Add To do list

---
+++ TOdo_12032020.m
@@ -0,0 +1,28 @@
+%% CLETE suggestions
+
+% 1. Arousal number button [per epoch]
+% 2. Grid lines for 1 second increments
+% 3. 60, 120, 180, 240 second EPOCHs
+% 4. Text field for free text [per epoch]
+
+
+%% MICHAEL suggestions
+
+% 1. Key commands for advancing EPOCHS [arrows]
+% 2. Key commands for entering SLEEP SCORES [ 1, 2, 3, R, W]
+% 3. Change 'A' to 'W'
+% 4. Zoom - #3 above
+% 4AA
+%          If I’ve selected anything other than 30, 
+%          I can’t score, but I can cycle through the 
+%          epochs at the longer time intervals.  For example, 
+%          if I have 120 selected, that’s 2 minutes of scoring.  
+%          There should be 4 sleep stages in that time period.  
+%          If it’s already been scored then displaying the stage 
+%          for those 4 sections (perhaps superimposed in a 
+%          semi-transparent large number across the epoch) 
+%          would be helpful.  If I hit the forward button 
+%          while in 120s epoch mode, then it would forward 
+%          the display showing the next 2 minutes of data.  
+% 5. Enlarge GUI
+% 6. CLICK on hypnogram
