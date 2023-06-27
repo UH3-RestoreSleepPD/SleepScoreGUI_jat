eea5ef6a0d8ea9599261c01f9067931e97ca81c6
John Thompson <john.a.thompson@cuanschutz.edu>
2023-06-26 21:28:53 -0600

    update files

---
+++ RCpS_Accel_OOBed/Accel_PROC_Steps.m
@@ -0,0 +1,14 @@
+% Create function to Extract Blocks
+
+% Create function to Extract Trials
+
+% Create function to Align Trials
+
+% Create function to generate PCA plot
+
+
+% Insert fake data between trials
+
+
+
+% Create algorithm to search data for matches.
