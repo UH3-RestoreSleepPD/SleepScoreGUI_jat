aa1e4c929456ce0abdb1a448206d49897b78bba0
John Thompson <john.a.thompson@cuanschutz.edu>
2023-07-09 21:36:58 -0600

    update

---
+++ RCpS_Accel_OOBed/STEPS_for_ACCEL_Proc_0702023.m
@@ -0,0 +1,10 @@
+%% STEPS for ACCEL PROCESSING
+
+% Process RAW data
+% use blockTrialExtract_as
+
+recDataLoc = 'C:\Users\Admin\Documents\Github\SleepScoreGUI_jat\RCpS_Accel_OOBed\Thompson_0622\Trial_1';
+
+blockTrialExtract_as(recDataLoc , NaN)
+
+%%
