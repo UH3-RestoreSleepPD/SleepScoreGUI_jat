213bb0e377bf1ac670fa01f328210f883789bd52
John A. Thompson <john.a.thompson@cuanschutz.edu>
2023-07-25 21:31:38 -0600

    update

--- RCpS_Accel_OOBed/STEPS_for_ACCEL_Proc_0702023.m
+++ RCpS_Accel_OOBed/STEPS_for_ACCEL_Proc_0702023.m
@@ -3,13 +3,28 @@
 % Process RAW data
 % use blockTrialExtract_as
 close all
-rLC = 'C:\Users\Admin\Documents\Github\SleepScoreGUI_jat\RCpS_Accel_OOBed\Thompson_0622\Trial_1';
+rLC = 'C:\Users\johna\OneDrive\Documents\Github\SleepScoreGUI_jat\RCpS_Accel_OOBed\Thompson_0622\Trial_1';
 
 blockTrialExtract_as(rLC,2,0,1)
 
 %%
 
 close all
-rLC2 = 'C:\Users\Admin\Documents\Github\SleepScoreGUI_jat\RCpS_Accel_OOBed\Platt_0622';
+rLC2 = 'C:\Users\johna\OneDrive\Documents\Github\SleepScoreGUI_jat\RCpS_Accel_OOBed\Platt_0622';
+
+blockTrialExtract_as(rLC2,2,0,1)
+
+%%
+
+close all
+rLC2 = 'C:\Users\johna\OneDrive\Documents\Github\SleepScoreGUI_jat\RCpS_Accel_OOBed\QuickTESTjat';
+
+blockTrialExtract_as(rLC2,2,0,1)
+
+
+%%
+
+close all
+rLC2 = 'C:\Users\johna\OneDrive\Documents\Github\SleepScoreGUI_jat\RCpS_Accel_OOBed\Machnika';
 
 blockTrialExtract_as(rLC2,2,0,1)
